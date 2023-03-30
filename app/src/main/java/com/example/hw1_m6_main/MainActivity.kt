d9898da06f00d450b8d4020e61ef04a6319d258b
ayakend <ayakend@gmail.com>
2023-03-30 21:02:08 +0600

    работает корректно

--- app/src/main/java/com/example/hw1_m6_main/MainActivity.kt
+++ app/src/main/java/com/example/hw1_m6_main/MainActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hw1_m6_main.DuplicateActivity.Companion.DU_MA_TEXT
@@ -12,12 +11,12 @@ import com.example.hw1_m6_main.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val resultLauncher: ActivityResultLauncher<Intent> by lazy {
+    private val resultLauncher=
         registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
             if (activityResult.resultCode == Activity.RESULT_OK && activityResult.data != null) {
                 binding.etFirstText.setText(activityResult.data!!.getStringExtra(DU_MA_TEXT))
             }
-        }
+
     }
     private lateinit var binding: ActivityMainBinding
 
